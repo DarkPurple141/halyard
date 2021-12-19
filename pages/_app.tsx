52baf13d1a0a0731f804574089b660f99f42104e
Alex Hinds <alex.hinds141@gmail.com>
2021-12-19 17:12:45 +1100

    chore: update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,19 +9,28 @@ function MyApp({ Component, pageProps }: any) {
     <Grid>
       <Head>
         <title>Halyard Software Consulting</title>
-        <meta name="description" content="Halyard is an Australia-based software solutions consultancy." />
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://halyard.dev/"/>
+        <meta
+          name="description"
+          content="Halyard is an Australia-based software solutions consultancy."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://halyard.dev/" />
         <meta property="og:title" content="Halyard Software Consulting" />
-        <meta property="og:description" content="Halyard is a boutique software and design consultancy helping to empower growing companies." />
+        <meta
+          property="og:description"
+          content="Halyard is a boutique software and design consultancy helping to empower growing companies."
+        />
         <meta property="og:image" content="/preview.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://halyard.dev/" />
         <meta property="twitter:title" content="Halyard Software Consulting" />
-        <meta property="twitter:description" content="Halyard is a boutique software and design consultancy helping to empower growing companies." />
+        <meta
+          property="twitter:description"
+          content="Halyard is a boutique software and design consultancy helping to empower growing companies."
+        />
         <meta property="twitter:image" content="/preview.png" />
-                
+
         <link rel="icon" href="/favicon.ico" />
         <script async src="https://cdn.splitbee.io/sb.js" />
       </Head>
